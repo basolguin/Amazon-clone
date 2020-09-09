b3b0311d7badc707755fa42c9a2e4f80d902bee5
basolguin <bas.olguin@gmail.com>
2020-09-09 01:50:44 -0300

    Crear aplicación React, con algo de contenido, Componente y CSS Header, incluyendo firebase SDK

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB3cBzIqbLW5w6z1TBYmFf9lopvdjQecwA",
+    authDomain: "clone-89d97.firebaseapp.com",
+    databaseURL: "https://clone-89d97.firebaseio.com",
+    projectId: "clone-89d97",
+    storageBucket: "clone-89d97.appspot.com",
+    messagingSenderId: "740610260283",
+    appId: "1:740610260283:web:d358efba9b9db7c0b1cb1e",
+    measurementId: "G-B1W4VSNXL0"
+  };
