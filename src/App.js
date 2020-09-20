ae42494b731334100961f36e2fd396079267b416
basolguin <bas.olguin@gmail.com>
2020-09-20 19:26:15 -0300

    Creación del front-end de la pagina Login

--- src/App.js
+++ src/App.js
@@ -4,24 +4,29 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     // Metodologia BEM
     <Router>
       <div className="app">
-        <Header />
-
         {/* Switch como en java c# etc */}
         {/* Asegurarse de mantener la por defecto '/' al final */}
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
 
           {/* esta es nuestra ruta por defecto, si se dirige a una ruta X 
           y esta no coincide con las especificadas nos devolverá por defecto '/' */}
           <Route path="/">
+            <Header />
             {/* Cuando nos encontramos en la ruta '/', renderizar Home*/}
             <Home />
           </Route>
