194bf67799adc8d622ca0c68288001f619e0bf60
basolguin <bas.olguin@gmail.com>
2020-09-19 21:25:35 -0300

    Listar productos en Checkout, y Eliminación de productos dentro de la canasta

--- src/reducer.js
+++ src/reducer.js
@@ -3,16 +3,41 @@ export const initialState = {
   basket: [],
 };
 
+// Selector (good practice)
+// Distinto/Mejor Método para obtener la suma total
+// export const getBasketTotal = (basket) =>
+// basket?.reduce( ( amount, item ) => item.price + amount, 0);
+//we add basket? as optional chaining when there is no objects
+//itll know to not go into the function
+
 //In order to write the Reducer-Crear reducer
 //we have a state of the application and action (are you trying to add to the basket?,remove, update?)
 const reducer = (state, action) => {
-    //vemos en la consola que recibe tal accion al clickear add to basket
+  //vemos en la consola que recibe tal accion al clickear add to basket
   console.log(action);
+
   switch (action.type) {
     case "ADD_TO_BASKET":
       return {
         ...state,
         basket: [...state.basket, action.item],
+      };
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as its not in basket!`
+        );
+      }
+      return {
+        ...state,
+        basket: newBasket,
       };
 
     //debemos tener nuestro default:
